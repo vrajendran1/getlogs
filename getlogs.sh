e2db32bea01c87ddfe6bd3439330618f5d42c722
vrajendran1 <121140432+vrajendran1@users.noreply.github.com>
2022-12-22 14:18:02 +0800

    added extra param intake for mode
    param 2 = 1 -> will split logs into its respective pods
    param 2 = any -> all pod logs will be in same file

--- getlogs.sh
+++ getlogs.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
 param=$1
+mode=$2
 pod_array=($(kubectl -n ${param} get pods --no-headers -o custom-columns=":metadata.name"))
 
 # declare -p pod_array -> to view array #
 
-# loop thru pods for logs #
-for i in "${pod_array[@]}"
-do
-        :
-        echo "###### $i" >> ${param}__${i}.txt
-        echo "####" >> ${param}__${i}.txt
-        echo "##" >> ${param}__${i}.txt
-        echo "#" >> ${param}__${i}.txt
-        echo "----------------------------------------" >> ${param}__${i}.txt
-        kubectl -n $param logs $i >> ${param}__${i}.txt
-        echo "----------------------------------------" >> ${param}__${i}.txt
-done
+
+if [[ "$mode" == "1" ]]
+then
+        # loop thru pods for logs #
+        for i in "${pod_array[@]}"
+        do
+                :
+                echo "###### $i" >> ${param}__${i}.txt
+                echo "####" >> ${param}__${i}.txt
+                echo "##" >> ${param}__${i}.txt
+                echo "#" >> ${param}__${i}.txt
+                echo "----------------------------------------" >> ${param}__${i}.txt
+                kubectl -n $param logs $i >> ${param}__${i}.txt
+                echo "----------------------------------------" >> ${param}__${i}.txt
+        done
+else
+        for i in "${pod_array[@]}"
+        do
+                :
+                echo "###### $i" >> ${param}.txt
+                echo "####" >> ${param}.txt
+                echo "##" >> ${param}.txt
+                echo "#" >> ${param}.txt
+                echo "----------------------------------------" >> ${param}.txt
+                kubectl -n $param logs $i >> ${param}.txt
+                echo "----------------------------------------" >> ${param}.txt
+        done
+fi
